1dc597de7f09d42af9561a0c4158ed765ef1f1ab
gabibecher <43253045+gabibecher@users.noreply.github.com>
2024-07-24 21:40:32 -0600

    frontend integrated w/ backend render deployment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,55 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import './App.css';
-import DataTableComponent from './DataTable';
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
+import DataTableComponent from "./DataTable";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const [data, setData] = useState([]);
   const [filteredData, setFilteredData] = useState([]);
-  const [searchInput, setSearchInput] = useState('');
-  const [error, setError] = useState('');
+  const [searchInput, setSearchInput] = useState("");
+  const [error, setError] = useState("");
 
+  // useEffect(() => {
+  //   axios.get('/data')
+  //     .then(response => {
+  //       console.log('API Response:', response.data);
+  //       if (response.data && Array.isArray(response.data.Items)) {
+  //         setData(response.data.Items);
+  //         setFilteredData(response.data.Items);
+  //         console.log(response.data.Items);
+  //       } else {
+  //         console.error('Unexpected data format:', response.data);
+  //         setError('Unexpected data format received from the server.');
+  //       }
+  //     })
+  //     .catch(error => {
+  //       setError('Error fetching data');
+  //       console.error('Error fetching data:', error);
+  //     });
+  // }, []);
+
+  // backend deployed through render
   useEffect(() => {
-    axios.get('/data')
-      .then(response => {
-        console.log('API Response:', response.data);
+    axios
+      .get("https://afghan-database.onrender.com/data")
+      .then((response) => {
+        console.log("API Response:", response.data);
         if (response.data && Array.isArray(response.data.Items)) {
           setData(response.data.Items);
           setFilteredData(response.data.Items);
-          console.log(response.data.Items);
         } else {
-          console.error('Unexpected data format:', response.data);
-          setError('Unexpected data format received from the server.');
+          console.error("Unexpected data format:", response.data);
+          setError("Unexpected data format received from the server.");
         }
       })
-      .catch(error => {
-        setError('Error fetching data');
-        console.error('Error fetching data:', error);
+      .catch((error) => {
+        setError("Error fetching data");
+        console.error("Error fetching data:", error);
       });
   }, []);
 
-  const handleChange = event => {
+  const handleChange = (event) => {
     const value = event.target.value;
     setSearchInput(value);
   };
@@ -42,16 +62,20 @@ function App() {
 
   const containsSubstring = (list, input) => {
     const lowerCaseInput = input.toLowerCase();
-    return list.some(item => item && item.toLowerCase().includes(lowerCaseInput));
+    return list.some(
+      (item) => item && item.toLowerCase().includes(lowerCaseInput)
+    );
   };
 
   const globalSearch = (input, data) => {
     if (input) {
-      const filtered = data.filter(row => {
+      const filtered = data.filter((row) => {
         return (
           row.summary.toLowerCase().includes(input.toLowerCase()) ||
           row.source_name.toLowerCase().includes(input.toLowerCase()) ||
-          row.country_of_publication.toLowerCase().includes(input.toLowerCase()) ||
+          row.country_of_publication
+            .toLowerCase()
+            .includes(input.toLowerCase()) ||
           containsSubstring(row.author, input) ||
           containsSubstring(row.associated_orgs, input) ||
           containsSubstring(row.type_of_pub, input) ||
@@ -68,10 +92,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1 className="App-title">Afghanistan Post-2021 Research Database</h1>
-        <p className="App-subtitle">A comprehensive collection of research articles and publications</p>
+        <p className="App-subtitle">
+          A comprehensive collection of research articles and publications
+        </p>
       </header>
       <div className="container mt-4">
-        {error && <div className="alert alert-danger" role="alert">{error}</div>}
+        {error && (
+          <div className="alert alert-danger" role="alert">
+            {error}
+          </div>
+        )}
         <div className="row mb-4">
           <div className="col-md-4">
             <input
@@ -85,12 +115,16 @@ function App() {
             />
           </div>
           <div className="col-md-2">
-              <button onClick={handleSearch} className="btn btn-primary">Apply Search</button>
-            </div>
+            <button onClick={handleSearch} className="btn btn-primary">
+              Apply Search
+            </button>
+          </div>
         </div>
         <div className="row mb-4">
           <div className="col-md-12">
-            <DataTableComponent data={filteredData.length ? filteredData : data} />
+            <DataTableComponent
+              data={filteredData.length ? filteredData : data}
+            />
           </div>
         </div>
       </div>
